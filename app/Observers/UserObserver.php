c0b5e916c4e7bde579d0183f735268df95206d88
Aliff <aliff.rosli96@gmail.com>
2021-07-30 16:30:00 +0800

    Observer Updated

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,19 +3,15 @@
 namespace App\Observers;
 
 use App\Models\User;
+use Illuminate\Support\Str;
 
 class UserObserver
 {
-    /**
-     * Handle the User "created" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    public function created(User $user)
+    public function creating(User $user)
     {
-        //
+        $user->uuid=Str::uuid();
     }
+   
 
     /**
      * Handle the User "updated" event.
