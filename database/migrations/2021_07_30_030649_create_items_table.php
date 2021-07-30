f44fe5b1200d5f3023a263da4cff99ff3f94d294
Aliff <aliff.rosli96@gmail.com>
2021-07-30 11:19:41 +0800

    first commit

---
+++ database/migrations/2021_07_30_030649_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('name')->nullable();
+            $table->string('description')->nullable();
+            $table->string('type')->nullable();
+            $table->integer('amount')->default('5000');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
